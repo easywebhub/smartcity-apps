ab144a16e05e085d364b594bedeb56d1deb9faef
quocbao_home <quocbao.tn@gmail.com>
2018-05-12 15:16:00 +0700

    cập nhật thêm đăng ký, giới thiệu

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 var $$ = Dom7;
 
 // Framework7 App main instance
-var app  = new Framework7({
+var app = new Framework7({
   root: '#app', // App root element
   id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
+  name: 'VinaAS', // App name
   theme: 'auto', // Automatic theme detection
   // App root data
   data: function () {
@@ -62,29 +62,56 @@ axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
 
 app.on('pageInit', function (page) {
   // localStorage.isAuthenticated = 'false'  ; //để debug
-  if (localStorage.isAuthenticated == 'true')  {
+  if (localStorage.isAuthenticated == 'true') {
     //đã đăng nhập
-  }else {
+  } else {
     //chưa đăng nhập: có thể hiện ra login
     app.loginScreen.open('#my-login-screen');
   }
 });
 
+$$('#my-signup-screen .signup-button').on('click', function () {
 
-// Login Screen Demo
+  var formData = app.form.convertToData('#form-signup');
+  if (!formData.username || !formData.password) {
+    app.dialog.alert("Hãy điền Tên và Mật Khẩu của bạn")
+    return;
+  }
+  if (!formData.email) {
+    formData.email = formData.username + '@vinaas.com';
+  }
+  //easyweb gọi api để cập nhật dữ liệu, lưu ở formData
+  console.log(JSON.stringify(formData));
+  app.dialog.alert(JSON.stringify(formData))
+  return;
+  axios.post('/Users', formData)
+    .then(function (response) {
+        console.log('Đăng Ký thành công')
+      localStorage.isAuthenticated = 'true';
+      localStorage.username = data.username;
+      localStorage.accessToken = data.id;
+      localStorage.userId = data.userId;
+      axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
+    })
+    .catch(function (error) {
+      app.dialog.alert("Đăng Ký không thành công do tên bị trùng! Hãy thử lại với tên khác")
+      return;
+    })
+
+    app.popup.close('#my-signup-screen');
+    app.loginScreen.close('#my-login-screen'); //Bao: đóng luôn popup đăng nhập đã mở nếu có
+});
+
 $$('#my-login-screen .login-button').on('click', function () {
   var username = $$('#my-login-screen [name="username"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
-
   //easyweb
   login();
 
-  // Alert username and password
-  //app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
 
 //Easyweb
-function login(){
+function login() {
   var username = $$('#my-login-screen [name="username"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
 
@@ -92,58 +119,58 @@ function login(){
   var credentials = { username: username, password: password }; //đặt tên là credentials: thông tin bảo mật
 
   axios.post('/Users/login', credentials)
-  .then(function (response) {
-    console.log(response.data)
-    if (response.data)  {
-      var data = response.data; //dữ liệu trả về bởi loopback3
-      localStorage.isAuthenticated = 'true' ; //là string, ko phải bool
-      localStorage.accessToken = data.id;
-      localStorage.userId = data.userId;
-      localStorage.ttl = data.ttl; //ttl: time to lease, thời gian mà accesstoken hết hạn
+    .then(function (response) {
+      console.log(response.data)
+      if (response.data) {
+        var data = response.data; //dữ liệu trả về bởi loopback3
+        localStorage.isAuthenticated = 'true'; //là string, ko phải bool
+        localStorage.accessToken = data.id;
+        localStorage.userId = data.userId;
+        localStorage.ttl = data.ttl; //ttl: time to lease, thời gian mà accesstoken hết hạn
 
-      axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
+        axios.defaults.headers.common['Authorization'] = localStorage.accessToken;
 
-      getUserInfo();
-      // Close login screen
-      app.loginScreen.close('#my-login-screen');
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+        getUserInfo();
+        // Close login screen
+        app.loginScreen.close('#my-login-screen');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 function getUserInfo() {
   console.log('getUserInfo()')
-  if (localStorage.isAuthenticated !== 'true')  {
+  if (localStorage.isAuthenticated !== 'true') {
     //lấy thông tin đầy đủ của user
     axios.get('/Users/' + localStorage.userId)
-    .then(function(response){
-      console.log(response.data)
-      var data = response.data;
-      localStorage.username = data.username;
-      localStorage.email = data.email;
-      localStorage.isAuthenticated = 'true'
-    })
-    .catch(function(error) {
-      localStorage.userId = '';
-      localStorage.isAuthenticated = 'false';
-    })
+      .then(function (response) {
+        console.log(response.data)
+        var data = response.data;
+        localStorage.username = data.username;
+        localStorage.email = data.email;
+        localStorage.isAuthenticated = 'true'
+      })
+      .catch(function (error) {
+        localStorage.userId = '';
+        localStorage.isAuthenticated = 'false';
+      })
     return true;
-  }else {
+  } else {
     return false;
   }
 }
 
-function getNews(){
+function getNews() {
   axios.get('/BaiViets', {
     params: {
       trangThai: PUBLISHED
     }
   })
-  .then(function (response) {
-    console.log('get news', response);
-  })
-  .catch(function (error) {
-    console.log('get news', error);
-  });
+    .then(function (response) {
+      console.log('get news', response);
+    })
+    .catch(function (error) {
+      console.log('get news', error);
+    });
 }
